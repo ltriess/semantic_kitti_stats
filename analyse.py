22005dd2e6c396b79e710e32abfe85f97c13ad56
ltriess <mail@triess.eu>
2020-10-04 13:12:57 +0200

    Add pre-commit config and fix styling

--- analyse.py
+++ analyse.py
@@ -4,69 +4,99 @@
 """Separate analysis of Semantic KITTI sequences and total statistics."""
 
 __author__ = """Larissa Triess"""
-__email__ = 'larissa@triess.eu'
+__email__ = "larissa@triess.eu"
 
 import os
 import sys
 
-import numpy as np
 import click
-
-from data import distance_bins, azimuth_bins, elevation_bins, load_data_points, load_data_labels, \
-    get_num_learning_labels, map_learning
-from utils import plots, get_distance_label_stats, get_angle_label_stats
+import numpy as np
+from data import (
+    azimuth_bins,
+    distance_bins,
+    elevation_bins,
+    get_num_learning_labels,
+    load_data_labels,
+    load_data_points,
+    map_learning,
+)
+from utils import get_angle_label_stats, get_distance_label_stats, plots
 
 
 @click.command()
-@click.argument('path', type=click.Path(exists=True))
-@click.option('--mode', type=click.Choice(['compute', 'from_data']), default='compute',
-              help='If compute is selected, PATH must be the path to the dataset. All statistics '
-                   'will be calculated from the data. If from_data is selected, PATH must be a '
-                   'a folder in which csv files with the computed statistics are located.')
-@click.option('--save_dir', type=click.Path(dir_okay=True),
-              help='Path where to save the generated graphs. If not provided, show on display.')
+@click.argument("path", type=click.Path(exists=True))
+@click.option(
+    "--mode",
+    type=click.Choice(["compute", "from_data"]),
+    default="compute",
+    help="If compute is selected, PATH must be the path to the dataset. All statistics "
+    "will be calculated from the data. If from_data is selected, PATH must be a "
+    "a folder in which csv files with the computed statistics are located.",
+)
+@click.option(
+    "--save_dir",
+    type=click.Path(dir_okay=True),
+    help="Path where to save the generated graphs. If not provided, show on display.",
+)
 def analyse(path, mode, save_dir):
 
-    sequence_folder = os.path.join(path, 'dataset', 'sequences')
+    sequence_folder = os.path.join(path, "dataset", "sequences")
 
     # placeholder for overall stats
-    pplds = np.zeros([11, get_num_learning_labels(), len(distance_bins)], dtype=np.int64)
+    pplds = np.zeros(
+        [11, get_num_learning_labels(), len(distance_bins)], dtype=np.int64
+    )
 
     # iterate over all train/val sequences
     for s in range(11):
-        if mode == 'compute':
-            print('\nSequence {}'.format(s))
+        if mode == "compute":
+            print("\nSequence {seq}".format(seq=s))
 
-            point_dir = os.path.join(sequence_folder, '{:02d}'.format(s), 'velodyne')
-            label_dir = os.path.join(sequence_folder, '{:02d}'.format(s), 'labels')
+            point_dir = os.path.join(sequence_folder, "{0:02d}".format(s), "velodyne")
+            label_dir = os.path.join(sequence_folder, "{0:02d}".format(s), "labels")
 
             if not os.path.exists(point_dir):
-                raise NotADirectoryError('Directory for points does not exits: {}'.format(point_dir))
+                raise NotADirectoryError(
+                    "Directory for points does not exits: {dir}".format(dir=point_dir)
+                )
             if not os.path.exists(label_dir):
-                raise NotADirectoryError('Directory for labels does not exits: {}'.format(label_dir))
+                raise NotADirectoryError(
+                    "Directory for labels does not exits: {dir}".format(dir=label_dir)
+                )
             if save_dir is not None and not os.path.exists(save_dir):
                 os.makedirs(save_dir)
 
             points_per_distance = np.zeros(len(distance_bins), dtype=np.int64)
             points_per_label = np.zeros(get_num_learning_labels(), dtype=np.int64)
             points_per_label_distance = np.zeros(
-                [get_num_learning_labels(), len(distance_bins)], dtype=np.int64)
+                [get_num_learning_labels(), len(distance_bins)], dtype=np.int64
+            )
             points_per_label_azimuth = np.zeros(
-                [get_num_learning_labels(), len(azimuth_bins)], dtype=np.int64)
+                [get_num_learning_labels(), len(azimuth_bins)], dtype=np.int64
+            )
             points_per_label_elevation = np.zeros(
-                [get_num_learning_labels(), len(elevation_bins) + 1], dtype=np.int64)
+                [get_num_learning_labels(), len(elevation_bins) + 1], dtype=np.int64
+            )
 
             point_filenames = sorted(os.listdir(point_dir))
             label_filenames = sorted(os.listdir(label_dir))
-            for c, (point_file, label_file) in enumerate(zip(point_filenames, label_filenames)):
-                sys.stdout.write('\r{:4d} / {:4d}'.format(c, len(point_filenames)))
+            for c, (point_file, label_file) in enumerate(
+                zip(point_filenames, label_filenames)
+            ):
+                sys.stdout.write("\r{0:4d} / {1:4d}".format(c, len(point_filenames)))
 
                 point_filename = os.path.join(point_dir, point_file)
                 label_filename = os.path.join(label_dir, label_file)
 
-                if not os.path.splitext(point_file)[0] == os.path.splitext(label_file)[0]:
-                    raise ValueError('Point list file {} does not match label file {}'.format(
-                        point_filename, label_filename))
+                if (
+                    not os.path.splitext(point_file)[0]
+                    == os.path.splitext(label_file)[0]
+                ):
+                    raise ValueError(
+                        "Point list file {pf} does not match label file {lf}".format(
+                            pf=point_filename, lf=label_filename
+                        )
+                    )
 
                 sensor, intensity = load_data_points(point_filename)
                 semantics, instance = load_data_labels(label_filename)
@@ -83,28 +113,59 @@ def analyse(path, mode, save_dir):
 
             # save the data
             if save_dir is not None:
-                np.savetxt(os.path.join(save_dir, '{:02d}_points_per_distance.csv'.format(s)),
-                           points_per_distance, delimiter=',')
-                np.savetxt(os.path.join(save_dir, '{:02d}_points_per_label.csv'.format(s)),
-                           points_per_label, delimiter=',')
-                np.savetxt(os.path.join(save_dir, '{:02d}_distance_label_matrix.csv'.format(s)),
-                           points_per_label_distance, delimiter=',')
-                np.savetxt(os.path.join(save_dir, '{:02d}_azimuth_label_matrix.csv'.format(s)),
-                           points_per_label_azimuth, delimiter=',')
-                np.savetxt(os.path.join(save_dir, '{:02d}_elevation_label_matrix.csv'.format(s)),
-                           points_per_label_elevation, delimiter=',')
+                np.savetxt(
+                    os.path.join(save_dir, "{0:02d}_points_per_distance.csv".format(s)),
+                    points_per_distance,
+                    delimiter=",",
+                )
+                np.savetxt(
+                    os.path.join(save_dir, "{0:02d}_points_per_label.csv".format(s)),
+                    points_per_label,
+                    delimiter=",",
+                )
+                np.savetxt(
+                    os.path.join(
+                        save_dir, "{0:02d}_distance_label_matrix.csv".format(s)
+                    ),
+                    points_per_label_distance,
+                    delimiter=",",
+                )
+                np.savetxt(
+                    os.path.join(
+                        save_dir, "{0:02d}_azimuth_label_matrix.csv".format(s)
+                    ),
+                    points_per_label_azimuth,
+                    delimiter=",",
+                )
+                np.savetxt(
+                    os.path.join(
+                        save_dir, "{0:02d}_elevation_label_matrix.csv".format(s)
+                    ),
+                    points_per_label_elevation,
+                    delimiter=",",
+                )
 
-        elif mode == 'from_data':
+        elif mode == "from_data":
             points_per_distance = np.loadtxt(
-                os.path.join(path, '{:02d}_points_per_distance.csv'.format(s)), delimiter=',')
+                os.path.join(path, "{0:02d}_points_per_distance.csv".format(s)),
+                delimiter=",",
+            )
             points_per_label = np.loadtxt(
-                os.path.join(path, '{:02d}_points_per_label.csv'.format(s)), delimiter=',')
+                os.path.join(path, "{0:02d}_points_per_label.csv".format(s)),
+                delimiter=",",
+            )
             points_per_label_distance = np.loadtxt(
-                os.path.join(path, '{:02d}_distance_label_matrix.csv'.format(s)), delimiter=',')
+                os.path.join(path, "{0:02d}_distance_label_matrix.csv".format(s)),
+                delimiter=",",
+            )
             points_per_label_azimuth = np.loadtxt(
-                os.path.join(path, '{:02d}_azimuth_label_matrix.csv'.format(s)), delimiter=',')
+                os.path.join(path, "{0:02d}_azimuth_label_matrix.csv".format(s)),
+                delimiter=",",
+            )
             points_per_label_elevation = np.loadtxt(
-                os.path.join(path, '{:02d}_elevation_label_matrix.csv'.format(s)), delimiter=',')
+                os.path.join(path, "{0:02d}_elevation_label_matrix.csv".format(s)),
+                delimiter=",",
+            )
         else:
             raise ValueError
 
@@ -113,27 +174,37 @@ def analyse(path, mode, save_dir):
         # visualize the data
         plots.draw_distance_and_label_matrix(
             points_per_label_distance,
-            save_dir=os.path.join(save_dir, '{:02d}_distance_label_matrix'.format(s))
-            if save_dir is not None else None)
+            save_dir=os.path.join(save_dir, "{0:02d}_distance_label_matrix".format(s))
+            if save_dir is not None
+            else None,
+        )
         plots.draw_points_per_distance(
             points_per_distance,
-            save_dir=os.path.join(save_dir, '{:02d}_points_per_distance.'.format(s))
-            if save_dir is not None else None)
+            save_dir=os.path.join(save_dir, "{0:02d}_points_per_distance.".format(s))
+            if save_dir is not None
+            else None,
+        )
         plots.draw_points_per_label(
             points_per_label,
-            save_dir=os.path.join(save_dir, '{:02d}_points_per_label'.format(s))
-            if save_dir is not None else None)
+            save_dir=os.path.join(save_dir, "{0:02d}_points_per_label".format(s))
+            if save_dir is not None
+            else None,
+        )
         plots.draw_azimuth_and_label_matrix(
             points_per_label_azimuth,
-            save_dir=os.path.join(save_dir, '{:02d}_azimuth_label_matrix'.format(s))
-            if save_dir is not None else None)
+            save_dir=os.path.join(save_dir, "{0:02d}_azimuth_label_matrix".format(s))
+            if save_dir is not None
+            else None,
+        )
         plots.draw_elevation_and_label_matrix(
             points_per_label_elevation,
-            save_dir=os.path.join(save_dir, '{:02d}_elevation_label_matrix'.format(s))
-            if save_dir is not None else None)
+            save_dir=os.path.join(save_dir, "{0:02d}_elevation_label_matrix".format(s))
+            if save_dir is not None
+            else None,
+        )
 
-    print('\n')
-    print('Finished all sequences. Starting total analysis...')
+    print("\n")
+    print("Finished all sequences. Starting total analysis...")
 
     ppld = np.sum(pplds, axis=0)  # total points per label distance
     ppl = np.sum(ppld, axis=1)  # total points per label
@@ -144,33 +215,61 @@ def analyse(path, mode, save_dir):
 
     # save the data
     if save_dir is not None:
-        np.savetxt(os.path.join(save_dir, 'total_distance_label_matrix.csv'), ppld, delimiter=',')
-        np.savetxt(os.path.join(save_dir, 'total_points_per_distance.csv'), ppd, delimiter=',')
-        np.savetxt(os.path.join(save_dir, 'total_points_per_label.csv'), ppl, delimiter=',')
+        np.savetxt(
+            os.path.join(save_dir, "total_distance_label_matrix.csv"),
+            ppld,
+            delimiter=",",
+        )
+        np.savetxt(
+            os.path.join(save_dir, "total_points_per_distance.csv"), ppd, delimiter=","
+        )
+        np.savetxt(
+            os.path.join(save_dir, "total_points_per_label.csv"), ppl, delimiter=","
+        )
 
-        np.savetxt(os.path.join(save_dir, 'sequence_distance_matrix.csv'), ppds, delimiter=',')
-        np.savetxt(os.path.join(save_dir, 'sequence_label_matrix.csv'), ppls, delimiter=',')
+        np.savetxt(
+            os.path.join(save_dir, "sequence_distance_matrix.csv"), ppds, delimiter=","
+        )
+        np.savetxt(
+            os.path.join(save_dir, "sequence_label_matrix.csv"), ppls, delimiter=","
+        )
 
     # visualize the data
     plots.draw_distance_and_label_matrix(
-        ppld, save_dir=os.path.join(save_dir, 'total_distance_label_matrix')
-        if save_dir is not None else None)
+        ppld,
+        save_dir=os.path.join(save_dir, "total_distance_label_matrix")
+        if save_dir is not None
+        else None,
+    )
     plots.draw_points_per_distance(
-        ppd, save_dir=os.path.join(save_dir, 'total_points_per_distance')
-        if save_dir is not None else None)
+        ppd,
+        save_dir=os.path.join(save_dir, "total_points_per_distance")
+        if save_dir is not None
+        else None,
+    )
     plots.draw_points_per_label(
-        ppl, save_dir=os.path.join(save_dir, 'total_points_per_label')
-        if save_dir is not None else None)
+        ppl,
+        save_dir=os.path.join(save_dir, "total_points_per_label")
+        if save_dir is not None
+        else None,
+    )
 
     plots.draw_distance_and_sequence_matrix(
-        ppds, save_dir=os.path.join(save_dir, 'sequence_distance_matrix')
-        if save_dir is not None else None)
+        ppds,
+        save_dir=os.path.join(save_dir, "sequence_distance_matrix")
+        if save_dir is not None
+        else None,
+    )
     plots.draw_label_and_sequence_matrix(
-        ppls, save_dir=os.path.join(save_dir, 'sequence_label_matrix')
-        if save_dir is not None else None)
+        ppls,
+        save_dir=os.path.join(save_dir, "sequence_label_matrix")
+        if save_dir is not None
+        else None,
+    )
     plots.draw_sequence_length(
-        save_dir=os.path.join(path, 'sequence_length') if save_dir is not None else None)
+        save_dir=os.path.join(path, "sequence_length") if save_dir is not None else None
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     analyse()
--- utils/plots.py
+++ utils/plots.py
@@ -1,33 +1,40 @@
 # -*- coding: utf-8 -*-
 
 __author__ = """Larissa Triess"""
-__email__ = 'larissa@triess.eu'
+__email__ = "larissa@triess.eu"
 
-import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+import matplotlib.pyplot as plt
+import numpy as np
+from data import (
+    azimuth_bins,
+    distance_bins,
+    elevation_bins,
+    get_names_learning,
+    get_num_learning_labels,
+)
 
-from data import distance_bins, azimuth_bins, elevation_bins, get_names_learning, get_num_learning_labels
 
-
-def draw_distance_and_label_matrix(matrix: np.array, save_dir: str=None):
+def draw_distance_and_label_matrix(matrix: np.array, save_dir: str = None):
     class_names, _, _ = get_names_learning()
 
     fig, ax = plt.subplots()
-    im = ax.pcolor(matrix, norm=colors.LogNorm(vmin=1, vmax=np.max(matrix)), cmap=plt.cm.Blues)
+    im = ax.pcolor(
+        matrix, norm=colors.LogNorm(vmin=1, vmax=np.max(matrix)), cmap=plt.cm.Blues
+    )
     cbar = ax.figure.colorbar(im, ax=ax)
-    cbar.ax.set_ylabel('Number of Points', rotation=90)
+    cbar.ax.set_ylabel("Number of Points", rotation=90)
 
-    ax.set_xlabel('Distance [m]')
+    ax.set_xlabel("Distance [m]")
     ax.set_xticks(np.arange(matrix.shape[1]))
-    ax.set_xticklabels(distance_bins, ha='right', rotation=45, rotation_mode='anchor')
-    ax.xaxis.set_label_position('bottom')
+    ax.set_xticklabels(distance_bins, ha="right", rotation=45, rotation_mode="anchor")
+    ax.xaxis.set_label_position("bottom")
     ax.xaxis.tick_bottom()
 
-    ax.set_ylabel('Class Label')
+    ax.set_ylabel("Class Label")
     ax.set_yticks(np.arange(matrix.shape[0] + 1))
-    ax.set_yticklabels(class_names, va='bottom')
-    ax.yaxis.set_label_position('left')
+    ax.set_yticklabels(class_names, va="bottom")
+    ax.yaxis.set_label_position("left")
     ax.yaxis.tick_left()
 
     plt.tight_layout()
@@ -39,18 +46,22 @@ def draw_distance_and_label_matrix(matrix: np.array, save_dir: str=None):
         plt.close()
 
 
-def draw_azimuth_and_label_matrix(matrix: np.array, save_dir: str=None):
+def draw_azimuth_and_label_matrix(matrix: np.array, save_dir: str = None):
     class_names, _, _ = get_names_learning()
 
     fig, ax = plt.subplots()
-    im = ax.pcolor(matrix, norm=colors.LogNorm(vmin=1, vmax=np.max(matrix)), cmap=plt.cm.Blues)
+    im = ax.pcolor(
+        matrix, norm=colors.LogNorm(vmin=1, vmax=np.max(matrix)), cmap=plt.cm.Blues
+    )
     cbar = ax.figure.colorbar(im, ax=ax)
-    cbar.ax.set_ylabel('Number of Points', rotation=90)
+    cbar.ax.set_ylabel("Number of Points", rotation=90)
 
-    ax.set_xlabel('Azimuth [degree]')
+    ax.set_xlabel("Azimuth [degree]")
     ax.set_xticks(np.arange(matrix.shape[1] + 1))
-    ax.set_xticklabels(np.append(azimuth_bins, 360), ha='right', rotation=45, rotation_mode='anchor')
-    ax.xaxis.set_label_position('bottom')
+    ax.set_xticklabels(
+        np.append(azimuth_bins, 360), ha="right", rotation=45, rotation_mode="anchor"
+    )
+    ax.xaxis.set_label_position("bottom")
     ax.xaxis.tick_bottom()
     for i, l in enumerate(ax.xaxis.get_ticklabels()):
         if i % 4 == 0:
@@ -58,10 +69,10 @@ def draw_azimuth_and_label_matrix(matrix: np.array, save_dir: str=None):
         else:
             l.set_visible(False)
 
-    ax.set_ylabel('Class Label')
+    ax.set_ylabel("Class Label")
     ax.set_yticks(np.arange(matrix.shape[0] + 1))
-    ax.set_yticklabels(class_names, va='bottom')
-    ax.yaxis.set_label_position('left')
+    ax.set_yticklabels(class_names, va="bottom")
+    ax.yaxis.set_label_position("left")
     ax.yaxis.tick_left()
 
     plt.tight_layout()
@@ -73,19 +84,20 @@ def draw_azimuth_and_label_matrix(matrix: np.array, save_dir: str=None):
         plt.close()
 
 
-def draw_elevation_and_label_matrix(matrix: np.array, save_dir: str=None):
+def draw_elevation_and_label_matrix(matrix: np.array, save_dir: str = None):
     class_names, _, _ = get_names_learning()
 
     fig, ax = plt.subplots()
-    im = ax.pcolor(matrix, norm=colors.LogNorm(vmin=1, vmax=np.max(matrix)), cmap=plt.cm.Blues)
+    im = ax.pcolor(
+        matrix, norm=colors.LogNorm(vmin=1, vmax=np.max(matrix)), cmap=plt.cm.Blues
+    )
     cbar = ax.figure.colorbar(im, ax=ax)
-    cbar.ax.set_ylabel('Number of Points', rotation=90)
+    cbar.ax.set_ylabel("Number of Points", rotation=90)
 
-    ax.set_xlabel('Elevation [degree]')
+    ax.set_xlabel("Elevation [degree]")
     ax.set_xticks(np.arange(matrix.shape[1]))
-    ax.set_xticklabels(elevation_bins, ha='right', rotation=45,
-                       rotation_mode='anchor')
-    ax.xaxis.set_label_position('bottom')
+    ax.set_xticklabels(elevation_bins, ha="right", rotation=45, rotation_mode="anchor")
+    ax.xaxis.set_label_position("bottom")
     ax.xaxis.tick_bottom()
     for i, l in enumerate(ax.xaxis.get_ticklabels()):
         if i % 4 == 0:
@@ -93,10 +105,10 @@ def draw_elevation_and_label_matrix(matrix: np.array, save_dir: str=None):
         else:
             l.set_visible(False)
 
-    ax.set_ylabel('Class Label')
+    ax.set_ylabel("Class Label")
     ax.set_yticks(np.arange(matrix.shape[0] + 1))
-    ax.set_yticklabels(class_names, va='bottom')
-    ax.yaxis.set_label_position('left')
+    ax.set_yticklabels(class_names, va="bottom")
+    ax.yaxis.set_label_position("left")
     ax.yaxis.tick_left()
 
     plt.tight_layout()
@@ -108,17 +120,17 @@ def draw_elevation_and_label_matrix(matrix: np.array, save_dir: str=None):
         plt.close()
 
 
-def draw_points_per_distance(ppd: np.array, save_dir: str=None):
+def draw_points_per_distance(ppd: np.array, save_dir: str = None):
 
     fig, ax = plt.subplots()
     plt.bar(np.arange(len(distance_bins)), ppd)
 
-    ax.set_xlabel('Distance [m]')
+    ax.set_xlabel("Distance [m]")
     ax.set_xticks(np.arange(len(distance_bins)) - 0.5)
     ax.set_xticklabels(distance_bins)
 
-    plt.ylabel('Number of Points')
-    plt.yscale('log')
+    plt.ylabel("Number of Points")
+    plt.yscale("log")
 
     plt.tight_layout()
 
@@ -129,17 +141,19 @@ def draw_points_per_distance(ppd: np.array, save_dir: str=None):
         plt.close()
 
 
-def draw_points_per_label(ppl: np.array, save_dir: str=None):
+def draw_points_per_label(ppl: np.array, save_dir: str = None):
 
     fig, ax = plt.subplots()
     plt.bar(np.arange(get_num_learning_labels()), ppl)
 
-    ax.set_xlabel('Class Label')
+    ax.set_xlabel("Class Label")
     ax.set_xticks(np.arange(get_num_learning_labels()))
-    ax.set_xticklabels(get_names_learning()[0], ha='right', rotation=45, rotation_mode='anchor')
+    ax.set_xticklabels(
+        get_names_learning()[0], ha="right", rotation=45, rotation_mode="anchor"
+    )
 
-    plt.ylabel('Number of Points')
-    plt.yscale('log')
+    plt.ylabel("Number of Points")
+    plt.yscale("log")
 
     plt.tight_layout()
 
@@ -150,24 +164,26 @@ def draw_points_per_label(ppl: np.array, save_dir: str=None):
         plt.close()
 
 
-def draw_distance_and_sequence_matrix(matrix: np.array, save_dir: str=None):
+def draw_distance_and_sequence_matrix(matrix: np.array, save_dir: str = None):
 
     fig, ax = plt.subplots()
-    im = ax.pcolor(matrix.T, norm=colors.LogNorm(vmin=1, vmax=np.max(matrix)), cmap=plt.cm.Blues)
+    im = ax.pcolor(
+        matrix.T, norm=colors.LogNorm(vmin=1, vmax=np.max(matrix)), cmap=plt.cm.Blues
+    )
     cbar = ax.figure.colorbar(im, ax=ax)
-    cbar.ax.set_ylabel('Number of Points', rotation=90)
+    cbar.ax.set_ylabel("Number of Points", rotation=90)
 
-    ax.set_xlabel('Sequence')
+    ax.set_xlabel("Sequence")
     ax.set_xticks(np.arange(matrix.shape[0]))
-    ax.set_xticklabels(['{:02d}'.format(i) for i in range(11)], ha='center')
+    ax.set_xticklabels(["{i:02d}".format(i=i) for i in range(11)], ha="center")
     ax.set_xticks([float(n) + 0.5 for n in ax.get_xticks()])
-    ax.xaxis.set_label_position('bottom')
+    ax.xaxis.set_label_position("bottom")
     ax.xaxis.tick_bottom()
 
-    ax.set_ylabel('Distance [m]')
+    ax.set_ylabel("Distance [m]")
     ax.set_yticks(np.arange(matrix.shape[1] + 1))
-    ax.set_yticklabels(distance_bins, va='center')
-    ax.yaxis.set_label_position('left')
+    ax.set_yticklabels(distance_bins, va="center")
+    ax.yaxis.set_label_position("left")
     ax.yaxis.tick_left()
 
     plt.tight_layout()
@@ -179,25 +195,27 @@ def draw_distance_and_sequence_matrix(matrix: np.array, save_dir: str=None):
         plt.close()
 
 
-def draw_label_and_sequence_matrix(matrix: np.array, save_dir: str=None):
+def draw_label_and_sequence_matrix(matrix: np.array, save_dir: str = None):
     class_names, _, _ = get_names_learning()
 
     fig, ax = plt.subplots()
-    im = ax.pcolor(matrix.T, norm=colors.LogNorm(vmin=1, vmax=np.max(matrix)), cmap=plt.cm.Blues)
+    im = ax.pcolor(
+        matrix.T, norm=colors.LogNorm(vmin=1, vmax=np.max(matrix)), cmap=plt.cm.Blues
+    )
     cbar = ax.figure.colorbar(im, ax=ax)
-    cbar.ax.set_ylabel('Number of Points', rotation=90)
+    cbar.ax.set_ylabel("Number of Points", rotation=90)
 
-    ax.set_xlabel('Sequence')
+    ax.set_xlabel("Sequence")
     ax.set_xticks(np.arange(matrix.shape[0]))
-    ax.set_xticklabels(['{:02d}'.format(i) for i in range(11)], ha='center')
+    ax.set_xticklabels(["{i:02d}".format(i=i) for i in range(11)], ha="center")
     ax.set_xticks([float(n) + 0.5 for n in ax.get_xticks()])
-    ax.xaxis.set_label_position('bottom')
+    ax.xaxis.set_label_position("bottom")
     ax.xaxis.tick_bottom()
 
-    ax.set_ylabel('Class Label')
+    ax.set_ylabel("Class Label")
     ax.set_yticks(np.arange(matrix.shape[1] + 1))
-    ax.set_yticklabels(class_names, va='bottom')
-    ax.yaxis.set_label_position('left')
+    ax.set_yticklabels(class_names, va="bottom")
+    ax.yaxis.set_label_position("left")
     ax.yaxis.tick_left()
 
     plt.tight_layout()
@@ -209,24 +227,46 @@ def draw_label_and_sequence_matrix(matrix: np.array, save_dir: str=None):
         plt.close()
 
 
-def draw_sequence_length(save_dir: str=None):
-    length = [4541, 1101, 4661, 801, 271, 2661, 1101, 1101, 4071, 1591, 1201,
-              921, 1061, 3281, 631, 1901, 1731, 491, 1801, 4981, 831, 2721]
+def draw_sequence_length(save_dir: str = None):
+    length = [
+        4541,
+        1101,
+        4661,
+        801,
+        271,
+        2661,
+        1101,
+        1101,
+        4071,
+        1591,
+        1201,
+        921,
+        1061,
+        3281,
+        631,
+        1901,
+        1731,
+        491,
+        1801,
+        4981,
+        831,
+        2721,
+    ]
 
     fig, ax = plt.subplots()
     barlist = plt.bar(np.arange(22), length)
 
-    ax.set_xlabel('Sequence')
+    ax.set_xlabel("Sequence")
     ax.set_xticks(np.arange(22))
-    ax.set_xticklabels(['{:02d}'.format(i) for i in range(22)], ha='center')
+    ax.set_xticklabels(["{i:02d}".format(i=i) for i in range(22)], ha="center")
 
-    plt.ylabel('Number of Frames')
+    plt.ylabel("Number of Frames")
 
-    barlist[8].set_color('m')
+    barlist[8].set_color("m")
     for i in range(11, 22):
-        barlist[i].set_color('c')
+        barlist[i].set_color("c")
 
-    plt.legend((barlist[0], barlist[8], barlist[11]), ('train', 'val', 'test'))
+    plt.legend((barlist[0], barlist[8], barlist[11]), ("train", "val", "test"))
 
     plt.tight_layout()
 
